3971bc0286ba5dea66046bc881409f1152d21cb8
Vadzim <vadzim0ganevich@gmail.com>
2020-04-09 22:50:37 +0300

    changed task6

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -1,9 +1,19 @@
 package subtask6
 
 class Fibonacci {
-
+    fun fibonacci(n: Int): Int {
+        if(n == 0 ) { return 0}
+        if(n == 1) { return 1}
+        return fibonacci(n-1) + fibonacci(n-2)
+    }
     // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
-    }
+
+
+        var counter = 0
+        while (fibonacci(counter) * fibonacci(counter+1) < n)
+        {counter++}
+        if(fibonacci(counter)*fibonacci(counter+1) == n) { return intArrayOf(fibonacci(counter),fibonacci(counter+1),1)}
+            else{return intArrayOf(fibonacci(counter),fibonacci(counter+1),0)}
+        }
 }
